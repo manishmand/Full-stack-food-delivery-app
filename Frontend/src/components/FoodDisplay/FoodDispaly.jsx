13c125d29b766dfe3b6914dd6bf551d6aa3ec5e5
Manish <mandalmanishjjjp@gmail.com>
2025-05-09 11:49:06 +0530

    Initial commit with frontend and backend setup

---
+++ Frontend/src/components/FoodDisplay/FoodDispaly.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/storeContext'
+import FoodItem from '../FoodItem/FoodItem'
+
+const FoodDispaly = ({ category}) => {
+
+   const {food_list} = useContext(StoreContext) 
+
+  return (
+    <div className='food-dispaly' id='food-dispaly'>
+        <h2>Top dishes near you</h2>
+        <div   className="food-display-list">
+            {food_list.map((item,index)=>{
+
+              if(category === "All" || category ===item.category){
+                return <FoodItem 
+                key={index} 
+                id={item._id} 
+                name={item.name} 
+                description={item.description}
+                 price={item.price}
+                 image={item.image}/>
+              }
+                
+            })}
+           
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default FoodDispaly
---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum obcaecati ipsa quam ut fuga veritatis enim. Deleniti incidunt quasi, odit, quos vero consequatur voluptatibus distinctio dolorum deserunt cum exercitationem nam?</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+
+                </ul>
+            </div>
+
+
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-22-34-5589</li>
+                    <li>contact@tomoto.com</li>
+                </ul>
+
+            </div>
+
+
+        </div>
+        <hr/>
+        <p className="footer-copyright">
+
+            Copyright 2024 @ Tomato.com - ALL Right Reserved.
+        </p>
+      
+    </div>
+  )
+}
+
+export default Footer
